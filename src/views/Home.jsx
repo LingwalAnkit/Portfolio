ac5a108e4fcd14adab836f5a3f8aa50a4ac09ddd
Ankit Lingwal <ankitlingwal090@gmail.com>
2025-01-17 13:54:09 +0530

    Resume

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,9 +4,9 @@ import Typical from "react-typical";
 import { contactLinks } from "../constants";
 import { ThemeContext } from "../themeProvider";
 import { motion } from "framer-motion";
-import { Link } from "react-scroll";
 import cloud from "../assets/cloudBg.png";
 import cloudDark from "../assets/cloudDark.png";
+import resume from "../assets/resume/Ankit.pdf"
 
 const Home = () => {
   const theme = useContext(ThemeContext);
@@ -67,11 +67,16 @@ const Home = () => {
               ))}
             </div>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-              <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
-                <Link className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
-                  Resume
-                </Link>
-              </div>
+            <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
+              <a
+                href={resume} // Replace with the actual path to your PDF file
+                download="Resume.pdf" // Optional: Sets the default download filename
+                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-400 md:py-4 md:text-lg md:px-10"
+              >
+              Resume
+            </a>
+          </div>
+
             </div>
           </div>
           <motion.img
